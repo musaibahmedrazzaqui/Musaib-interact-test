40ae5dbd5831b87245439ff30e9226e8d3bed269
musaibahmedrazzaqui <musaibahmed2000@gmail.com>
2023-08-11 16:45:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,40 @@ app.get("/", function (req, res) {
   console.log("SERVER STARTED");
   res.send("Server is running");
 });
+function formatDate(inputDate) {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const date = new Date(inputDate);
+  const dayOfWeek = daysOfWeek[date.getUTCDay()];
+  const day = date.getUTCDate();
+  const month = months[date.getUTCMonth()];
+  const year = date.getUTCFullYear();
+
+  return `${dayOfWeek}, ${day} ${month} ${year}`;
+}
+
 app.post("/getShipment", express.json(), function (req, res) {
   console.log(req.body);
   let orderId = req.body;
@@ -22,15 +56,17 @@ app.post("/getShipment", express.json(), function (req, res) {
     )
     .then((response) => {
       console.log(response.data.shipmentDate);
+
       appData["error"] = 0;
       appData["data"] = response.data.shipmentDate;
-      console.log("appdata", appData.data);
+      // console.log("appdata", appData.data);
+      let shipmentDate = formatDate(appData.data);
       const fulfilmentMsg = {
         fulfillmentMessages: [
           {
             card: {
-              title: "Shipment Date",
-              subtitle: appData.data,
+              title: shipmentDate,
+              subtitle: "Shipment Date",
               imageUri:
                 "https://static.vecteezy.com/system/resources/previews/006/900/704/original/green-tick-checkbox-illustration-isolated-on-white-background-free-vector.jpg",
               buttons: [
